abed8dafd93a0771bfdf6a33436e335af9114c34
mateusz-semklo <glazewo@gmail.com>
2023-10-12 12:06:56 +0200

    init

---
+++ src/main/java/pl/mateusz_semklo/automationshoprest/AutomationShopRestApplication.java
@@ -0,0 +1,13 @@
+package pl.mateusz_semklo.automationshoprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationShopRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationShopRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mateusz_semklo/automationshoprest/AutomationShopRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateusz_semklo.automationshoprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationShopRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
